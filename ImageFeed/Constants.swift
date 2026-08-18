b43bbcaadf723477e4e9c5fbb279503a66ab0eb5
Krios <kriosereb@gmail.com>
2026-08-18 07:00:28 +0500

    feat: add api constants

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Артем Бобриков on 18.08.2026.
+//
+
+enum Constants {
+    static let accessKey = "ztf8sqBbR2oeI0MsJcgUiVtV7CQ5PTjcYFGnFt8zOnM"
+    static let secretKey = "QyEnFSa-1U8wVNgJch8eKFfStqq3wWmTjO71ZbTf07Y"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURLString = "https://api.unsplash.com"
+}
