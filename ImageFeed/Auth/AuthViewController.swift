8c5ff3ac8acd04367869c27b293a3f6d21a8912c
Krios <kriosereb@gmail.com>
2026-08-20 11:03:22 +0500

    feat: add auth web view

---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,45 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by Артем Бобриков on 18.08.2026.
+//
+import UIKit
+
+final class AuthViewController: UIViewController {
+    private let showWebViewSegueIdentifier = "ShowWebView"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureBackButton()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == showWebViewSegueIdentifier {
+            guard let webViewViewController = segue.destination as? WebViewViewController else {
+                assertionFailure("Failed to prepare for \(showWebViewSegueIdentifier)")
+                return
+            }
+            
+            webViewViewController.delegate = self
+        } else {
+            super.prepare(for: segue, sender: sender)
+        }
+    }
+    
+    private func configureBackButton() {
+        navigationController?.navigationBar.backIndicatorImage = UIImage(named: "Backward")
+        navigationController?.navigationBar.backIndicatorTransitionMaskImage = UIImage(named: "Backward")
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        navigationItem.backBarButtonItem?.tintColor = UIColor(named: "YP Black")
+    }
+}
+
+extension AuthViewController: WebViewViewControllerDelegate {
+    func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
+    }
+    
+    func webViewViewControllerDidCancel(_ vc: WebViewViewController) {
+        dismiss(animated: true)
+    }
+}
